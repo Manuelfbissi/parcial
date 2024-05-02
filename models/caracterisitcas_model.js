bc5ee1a7717b0571facfc0e7e05230f5f960e9fd
Manuel Bissi <manuel.fernandez@davinci.edu.ar>
2024-05-01 22:10:29 -0300

    models-caracteristicas

---
+++ models/caracterisitcas_model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+
+const caracteristicasSchema = new mongoose.Schema({
+
+    datos_personaje: {
+        type: Schema.Types.ObjectId, ref: 'personajes'
+    },
+
+    Especie: {
+        type: String,
+        required: true
+    },
+    Planeta_origen: {
+        type: String,
+        required: true
+    },
+    Debut : {
+        type: String,
+        required: true
+    },
+    estado: {
+        type: Boolean,
+        required: true
+    },
+   
+    imagen: {
+        type: String,
+        required: false
+    }
+})
+
+export default mongoose.model("caracteristicas", caracteristicasSchema)
