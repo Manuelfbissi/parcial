1d115c6b0acc53c7ff33488a0a3e8c8105a94f73
Manuel Bissi <manuel.fernandez@davinci.edu.ar>
2024-05-06 01:47:28 -0300

    change index

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose"
 import "dotenv/config"
 import personajes_routes from "./routes/personaje_routes.js"
 import caracteristicas_routes from "./routes/caracteristicas_routes.js"
-// mongodb://127.0.0.1:27017/cursos
+//"mongodb://127.0.0.1:27017/personajes_rickAndMorty"
+//mongodb+srv://manuelfernandez:1234@cluster0.6dqlz9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 mongoose
-.connect ("mongodb://127.0.0.1:27017/personajes_rickAndMorty")
+.connect ("mongodb+srv://manuelfernandez:1234@cluster0.6dqlz9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log("conectado"))
     .catch(() => console.log("error al conectar"))
 
